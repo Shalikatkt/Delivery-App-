6ea31e472eef1ccb10ee2242ce09b121a3c5d9d5
Tanisha Shalika <162815099+Shalikatkt@users.noreply.github.com>
2025-02-25 11:38:00 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contenty">
+        <h2>Order Your Fvourite Fooed</h2>
+        <p>Craving your favorite meals? Get delicious food delivered straight to your doorstep with ease! Browse a variety of restaurants, place your order in just a few clicks, and enjoy fresh, hot, and tasty dishes anytime, anywhere. Fast, reliable, and hassle-free – because great food should never wait!</p>
+        <button>Menu</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
